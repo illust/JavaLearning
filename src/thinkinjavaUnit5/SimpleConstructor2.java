b1925208d2b814e6520473abe00dcd100e338a58
Sven Wong <illust@users.noreply.github.com>
2018-03-26 19:32:01 +0800

    daily work

--- src/thinkinjavaUnit5/SimpleConstructor2.java
+++ src/thinkinjavaUnit5/SimpleConstructor2.java
@@ -11,7 +11,9 @@ class Rock2{
 }
 
 public class SimpleConstructor2 {
-    public static void main(String[] args){
+    public static void
+
+    main(String[] args){
         for(int i = 0; i < 8; i++)
             new Rock2(i);
     }
--- src/tmp/progQuiz2.java
+++
@@ -1,9 +0,0 @@
-package tmp;
-
-/**
- * Created by Sven on 2018/3/3.
- * Find the greatest common divisor and the least common multiple.
- */
-public class progQuiz2 {
-
-}
